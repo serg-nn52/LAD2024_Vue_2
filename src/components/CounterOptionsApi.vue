8eb2376280affd77573b2b80b9f3bfeae495f490
Sergey Lushkin <serg.nn1985@gmail.com>
2024-12-10 22:10:07 +0300

    Добавил второй урок по Vue

---
+++ src/components/CounterOptionsApi.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="counter-wrapper">Counter Options Api: {{ count }}</div>
+  <div class="counter-wrapper">Double Count: {{ doubleCount }}</div>
+  <button @click="count++">Increment</button>
+  <button @click="decrementCount">Decrement</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CounterOptionsApi',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    decrementCount() {
+      this.count--;
+    },
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.counter-wrapper {
+  color: green;
+  font-size: 24px;
+}
+button {
+  font-size: 16px;
+  background-color: chartreuse;
+  border: none;
+  cursor: pointer;
+  padding: 10px 25px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CounterExample from '../components/CounterExample.vue';
+import CounterOptionsApi from '../components/CounterOptionsApi.vue';
+</script>
+
+<template>
+  <main>
+    <CounterExample />
+    <CounterOptionsApi />
+  </main>
+</template>
